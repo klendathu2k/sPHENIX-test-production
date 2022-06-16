71e7b3edcc26f7baf27bb62f9a22ca4529f9975b
klendathu2k <jwebb@bnl.gov>
2022-06-16 11:11:04 -0400

    [Shrek submission tag simple21]

--- simple21/bottom.sh
+++ simple21/bottom.sh
@@ -9,48 +9,20 @@ export name=bottom
 export comment=Simple example from panda
 
 
+
+# Clean up 
+rm /sphenix/u/sphnxpro/shrek/*
+
 echo ---------------------------------------------------------
 echo bottom.sh:
 cat bottom.sh
 echo ---------------------------------------------------------
 
 echo output:
-
 echo ${IN1[@]}
 echo ${IN2[@]}
 
-tar tvf ${IN1[@]}
-tar tvf ${IN2[@]}
-
-echo ========== ls -la 
-ls -la 
-
-echo ========== readlink -f *
-
-readlink -f *
-
-echo ========== ls -la `readlink -f *top1outDS*`    
-
-ls -la `readlink -f *top1outDS*`
-
-echo ========== ls -la just the path
-
-export fullpath=`readlink -f *top1outDS*`
-export dirpath=`dirname ${fullpath}`
-
-ls -la $dirpath
-
-echo ========== ls -l just the path plus the panda pilot directory
-
-ls -la $dirpath/PanDA*/
-
-echo ==========
-
-tar tvzf `readlink -f *top1outDS*`
-
-echo ==========
-
 touch results.root
 ls > results.root
-cp results.root /sphenix/u/sphnxpro/shrek/results-${uniqueId}
+#cp results.root /sphenix/u/sphnxpro/shrek/results-${uniqueId}
 cp _${name}.log /sphenix/u/sphnxpro/shrek/_${name}-${uniqueId}.log
