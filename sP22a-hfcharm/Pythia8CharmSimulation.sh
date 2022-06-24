61b56c2a7d2ec18aeb41deedc35f2565f4228ed4
klendathu2k <jwebb@bnl.gov>
2022-06-24 16:14:20 -0400

    [Shrek submission tag sP22a-hfcharm]

--- sP22a-hfcharm/Pythia8CharmSimulation.sh
+++ sP22a-hfcharm/Pythia8CharmSimulation.sh
@@ -15,7 +15,7 @@ export runnumber=1
 export maxAttempt=3
 
 
-echo START ${name} `date` 
+echo START ${name} `date` > /sphenix/u/sphnxpro/shrek/${name}-${uniqueId}.log1
 
 # Initialize sPHENIX software environment
 source /opt/sphenix/core/bin/sphenix_setup.sh -n ${build}
@@ -24,18 +24,20 @@ rn=$( printf "%010d" $runnumber )
 sn=$( printf "%05d"  $uniqueId )
 
 filename=G4Hits-${flavor}-${rn}-${sn}.root
-root.exe -q -b ${macro}\($nEvents,\"${flavor}\",\"${filename}\",\"\",0,\".\"\) >& runCharmSimu-${uniqueId}.funlog
-ls -la ${filename}
+root.exe -q -b ${macro}\($nEvents,\"${flavor}\",\"${filename}\",\"\",0,\".\"\) >> /sphenix/u/sphnxpro/shrek/${name}-${uniqueId}.log1
+ls -la ${filename} >> /sphenix/u/sphnxpro/shrek/${name}-${uniqueId}.log1
+
 
 # Add output to the tar file
 tar cvf Pythia8CharmSimulation.outDS.tar ${filename}
-tar xvf Pythia8CharmSimulation.outDS.tar
-ls -la ${filename}
+tar xvf Pythia8CharmSimulation.outDS.tar  >> /sphenix/u/sphnxpro/shrek/${name}-${uniqueId}.log1
+ls -la ${filename} >> /sphenix/u/sphnxpro/shrek/${name}-${uniqueId}.log1
 
-root.exe <<+++
+root.exe <<+++  
 TFile::Open( "${filename}" );
 gFile->ls();
 +++
 
 echo END   ${name} `date` 
+echo END   ${name} `date` >> /sphenix/u/sphnxpro/shrek/${name}-${uniqueId}.log1
 cp _${name}.log /sphenix/u/sphnxpro/shrek/${name}-${uniqueId}.log
