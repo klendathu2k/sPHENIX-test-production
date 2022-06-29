9e42752be9accab8f9b1bc7a0a17addeb487c16d
klendathu2k <jwebb@bnl.gov>
2022-06-29 18:15:10 -0400

    [Shrek submission tag sP22a-hfcharm]

--- sP22a-hfcharm/Pythia8CharmSimulation.sh
+++ sP22a-hfcharm/Pythia8CharmSimulation.sh
@@ -8,7 +8,7 @@ export name=Pythia8CharmSimulation
 export build=mdc2.7
 export comment=Generate Pythia8 Charm Events and run through F4A
 export nJobs=1
-export nEvents=10
+export nEvents=1
 export macro=Fun4All_G4_HF_pp_signal.C
 export flavor=Charm
 export runnumber=1
@@ -24,16 +24,15 @@ rn=$( printf "%010d" $runnumber )
 sn=$( printf "%05d"  $uniqueId )
 
 filename=G4Hits-${flavor}-${rn}-${sn}.root
-#root.exe -q -b ${macro}\($nEvents,\"${flavor}\",\"${filename}\",\"\",0,\".\"\) >> /sphenix/u/sphnxpro/shrek/${name}-${uniqueId}.log1
-#ls -la ${filename} >> /sphenix/u/sphnxpro/shrek/${name}-${uniqueId}.log1
-
+root.exe -q -b ${macro}\($nEvents,\"${flavor}\",\"${filename}\",\"\",0,\".\"\) >> /sphenix/u/sphnxpro/shrek/${name}-${uniqueId}.log1
+ls -la ${filename} >> /sphenix/u/sphnxpro/shrek/${name}-${uniqueId}.log1
 
 # Add output to the tar file
-#tar cvf Pythia8CharmSimulation.outDS.tar ${filename}
-#tar xvf Pythia8CharmSimulation.outDS.tar  >> /sphenix/u/sphnxpro/shrek/${name}-${uniqueId}.log1
-
-touch ${filename}
 tar cvf Pythia8CharmSimulation.outDS.tar ${filename}
+tar xvf Pythia8CharmSimulation.outDS.tar  >> /sphenix/u/sphnxpro/shrek/${name}-${uniqueId}.log1
+
+#touch ${filename}
+#tar cvf Pythia8CharmSimulation.outDS.tar ${filename}
 
 ls -la ${filename} >> /sphenix/u/sphnxpro/shrek/${name}-${uniqueId}.log1
 
--- sP22a-hfcharm/sP22a-hfcharm-workflow.cwl
+++ sP22a-hfcharm/sP22a-hfcharm-workflow.cwl
@@ -27,7 +27,7 @@ steps:
         opt_exec:
           default: "Pythia8MinbSimulation.sh  %RNDM:0 >& _Pythia8MinbSimulation.log "
         opt_args:
-          default: " --nJobs 10  --maxAttempt 3  --outputs Pythia8MinbSimulation.outDS.tar  --site BNL_OSG_SPHENIX --avoidVP --noBuild "
+          default: " --nJobs 1  --maxAttempt 3  --outputs Pythia8MinbSimulation.outDS.tar  --site BNL_OSG_SPHENIX --avoidVP --noBuild "
     out: [outDS]
 
   Pythia8CharmPileup:
@@ -42,5 +42,5 @@ steps:
         opt_exec:
           default: "Pythia8CharmPileup.sh  %RNDM:0 %IN %IN2 >& _Pythia8CharmPileup.log "
         opt_args:
-          default: " --maxAttempt 3  --outputs *.root  --nFilesPerJob=1 --secondaryDSs IN2:10:%{DS1} --forceStaged  --forceStagedSecondary  --site BNL_OSG_SPHENIX --avoidVP --noBuild "
+          default: " --maxAttempt 3  --outputs *.root  --nFilesPerJob=1 --secondaryDSs IN2:1:%{DS1} --forceStaged  --forceStagedSecondary  --site BNL_OSG_SPHENIX --avoidVP --noBuild "
     out: [outDS]
