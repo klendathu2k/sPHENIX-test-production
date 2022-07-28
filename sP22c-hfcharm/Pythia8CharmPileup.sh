1f1ad748181636219cbdd1dd7441bb56d6bd760b
klendathu2k <jwebb@bnl.gov>
2022-07-28 14:33:37 -0400

    [Shrek submission tag sP22c-hfcharm]

---
+++ sP22c-hfcharm/Pythia8CharmPileup.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+# Stage resources into working directory
+cp -R __Pythia8CharmPileup/* .
+echo $@
+
+echo Executing on `hostname`
+uname -a
+lscpu | grep \^CPU
+free -h --giga
+
+export uniqueId=$1
+export IN1_name=Pythia8CharmSimulation/outDS
+export IN1=(`echo $2 | tr "," " "`)
+export IN2_name=Pythia8MinbSimulation/outDS
+export IN2=(`echo $3 | tr "," " "`)
+export name=Pythia8CharmPileup
+export build=mdc2.8
+export comment=This is a test job definition
+export macro=Fun4All_G4_Pileup_pp.C
+export flavor=Charm
+export nEvents=1
+export nJobs=10
+export maxAttempt=3
+
+
+echo START ${name} `date` 
+
+# Initialize sPHENIX software environment
+source /opt/sphenix/core/bin/sphenix_setup.sh -n ${build}
+
+# Input file
+mcin=${IN1[0]}
+
+echo ARGV:       $@       
+echo Signal:     ${IN1[@]}  
+echo Background: ${IN2[@]}
+
+export signal=`tar xvf ${mcin}`
+
+touch background.list
+for i in "${IN2[@]}"
+do
+   tar xvf $i >> background.list
+done
+
+echo ======================================================================================
+echo Signal: ${signal}                 
+echo Background: `cat background.list` 
+
+echo ======================================================================================
+echo Directory contents before job execution
+ls -l 
+
+echo ======================================================================================
+echo Executing the pileup task...
+
+# echo arg1 \(output events\) : $1
+# echo arg2 \(input file\): $2
+# echo arg3 \(background listfile\): $3
+# echo arg4 \(output dir\): $4
+# echo arg5 \(quarkfilter\): $5
+# echo arg6 \(runnumber\): $6
+# echo arg7 \(sequence\): $7
+# root.exe -q -b Fun4All_G4_Pileup_pp.C\($1,\"$2\",\"$3\",\"$4\",\"$5\"\)
+
+root.exe -q -b ${macro}\(${nEvents},\"${signal}\",\"background.list\",\".\",\"${flavor}\"\)
+
+# Force successful job completion b/c PanDA
+touch DST_blah.root 
+
+tar cvf Pythia8Pileup.outDS.tar DST_*.root
+
+echo ======================================================================================
+echo Directory contents after job execution
+ls -l 
+
+echo END   ${name} `date` 
+
+cp _${name}.log /sphenix/u/sphnxpro/shrek/${name}-${uniqueId}.log
