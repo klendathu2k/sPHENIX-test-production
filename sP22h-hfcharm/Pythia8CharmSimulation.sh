10ee703395d257bace3221b693718de3b10f64ab
klendathu2k <jwebb@bnl.gov>
2022-07-29 14:39:32 -0400

    [Shrek submission tag sP22h-hfcharm]

---
+++ sP22h-hfcharm/Pythia8CharmSimulation.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Stage resources into working directory
+cp -R __Pythia8CharmSimulation/* .
+echo $@
+
+echo Executing on `hostname`
+uname -a
+lscpu | grep \^CPU
+free -h --giga
+
+export uniqueId=$1
+export name=Pythia8CharmSimulation
+export build=mdc2.8
+export comment=Generate Pythia8 Charm Events and run through F4A
+export nJobs=100
+export nEvents=1
+export macro=Fun4All_G4_HF_pp_signal.C
+export flavor=Charm
+export runnumber=1
+export maxAttempt=3
+export memory=8192
+
+
+echo START ${name} `date` 
+
+# Initialize sPHENIX software environment
+source /opt/sphenix/core/bin/sphenix_setup.sh -n ${build}
+
+echo Working directory
+pwd
+du --human-readable .
+df --human-readable .
+
+
+
+
+rn=$( printf "%010d" $runnumber )
+sn=$( printf "%05d"  $uniqueId )
+
+filename=G4Hits-${flavor}-${rn}-${sn}.root
+root.exe -q -b ${macro}\($nEvents,\"${flavor}\",\"${filename}\",\"\",0,\".\"\)
+
+
+# Add output to the tar file
+tar cvf Pythia8CharmSimulation.outDS.tar ${filename}
+ls -l
+
+echo END   ${name} `date` 
+
+cp _${name}.log /sphenix/u/sphnxpro/shrek/${name}-${uniqueId}.log
