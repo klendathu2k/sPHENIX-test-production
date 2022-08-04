6413c5c7bf6b62ae133790c030da33263781ae5e
klendathu2k <jwebb@bnl.gov>
2022-08-04 15:45:01 -0400

    [Shrek submission tag sP22n-hfcharm-background100]

---
+++ sP22n-hfcharm-background100/FinishMinbSimu.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Stage resources into working directory
+cp -R __FinishMinbSimu/* .
+echo $@
+
+echo Executing on `hostname`
+uname -a
+lscpu | grep \^CPU
+free -h --giga
+
+export uniqueId=$1
+export IN1_name=Pythia8MinbSimulation/outDS
+export IN1=(`echo $2 | tr "," " "`)
+export shrek_tag=sP22n-hfcharm-background100
+export name=FinishMinbSimu
+export build=mdc2.8
+export comment=This is a test job definition
+export maxAttempt=5
+
+
+echo START ${name} `date` 
+
+# Initialize sPHENIX software environment
+source /opt/sphenix/core/bin/sphenix_setup.sh -n ${build}
+
+
+# Input file
+mcin=${IN1[0]}
+
+echo ARGV:       $@       
+echo Signal:     ${IN1[@]}  
+
+tar xvf ${mcin} > FiniahPythia8MinbSimu.outDS.txt 
+
+echo END   ${name} `date` 
+
+cp _${name}.log /sphenix/u/sphnxpro/shrek/${shrek_tag}-${name}-${uniqueId}.log
