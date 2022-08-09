c4d70ac13687ff783ce0f1b9dcdd63d7433901b3
klendathu2k <jwebb@bnl.gov>
2022-08-08 20:38:49 -0400

    [Shrek submission tag sP22p-hfcharm-background100]

---
+++ sP22p-hfcharm-background100/Pythia8MinbSimulation.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# Stage resources into working directory
+cp -R __Pythia8MinbSimulation/* .
+echo $@
+
+echo Executing on `hostname`
+uname -a
+lscpu | grep \^CPU
+free -h --giga
+
+export uniqueId=$1
+export shrek_tag=sP22p-hfcharm-background100
+export name=Pythia8MinbSimulation
+export build=mdc2.8
+export comment=Generate Pythia8 Minb Events and run through F4A
+export nJobs=50
+export nEvents=100
+export macro=Fun4All_G4_Pass1_pp.C
+export flavor=ppminbias
+export runnumber=1
+export maxAttempt=3
+export memory=8192
+export maxWalltime=48
+export cpuTimePerEvent=75000
+
+
+echo START ${name} `date` 
+
+# Initialize sPHENIX software environment
+source /opt/sphenix/core/bin/sphenix_setup.sh -n ${build}
+
+which root
+root --version
+root-config --features
+
+rn=$( printf "%010d" $runnumber )
+sn=$( printf "%05d"  $uniqueId )
+
+# hepmc input data file
+ln -s /sphenix/sim/sim01/sphnxpro/MDC1/pythia8_HepMC/data/*-${sn}.dat
+hepmc=`ls *-${sn}.dat`
+
+filename=G4Hits-${flavor}-${rn}-${sn}.root
+
+root.exe -q -b ${macro}\($nEvents,\"${hepmc}\",\"${filename}\",\"\",0,\".\"\) 
+#touch $filename
+
+tar cvf Pythia8MinbSimulation.outDS.tar ${filename}  
+ls -l
+
+echo END   ${name} `date` 
+
+cp _${name}.log /sphenix/u/sphnxpro/shrek/${shrek_tag}-${name}-${uniqueId}.log
